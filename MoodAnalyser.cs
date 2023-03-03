8fadf243cf6c746dba3d7296ac949573e0738e80
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 05:00:51 +0530

    Refactor code using Constructor

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -8,6 +8,11 @@ namespace MoodAnalyzerProblem
 {
     public class MoodAnalyser
     {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
         public string AnalyzerMood(string message)
         {
             if (message.ToLower().Contains("sad"))
@@ -15,5 +20,7 @@ namespace MoodAnalyzerProblem
             else
             return "Happy";
         }
+
+        
     }
 }

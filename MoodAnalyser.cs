2dd473e3b8f4375e96514d0a829cef33ee0fa32e
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 04:39:24 +0530

    Analyse the mood for givin message

---
+++ MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyser
+    {
+        public string AnalyzerMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "Sad";
+            else
+            return "Happy";
+        }
+    }
+}

21d2f68c17396dac805a18256356187ee471636e
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 04:47:07 +0530

    Analyse the mood for givin message

---
+++ MoodTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using MoodAnalyzerProblem;
+namespace MoodTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMessage_WhenAnalyze_ShouldReturnMood()
+        {
+            //Arrang
+            string message = "I am in Sad Mood";
+            //Act
+            MoodAnalyser analyzer = new MoodAnalyser();
+            string actual = analyzer.AnalyzerMood(message);
+            //Assert
+            Assert.AreEqual(actual, "Sad");
+        }
+        [TestMethod]
+        public void GivenMessage_WhenAnalyze_ShouldReturnHappyMood()
+        {
+            //Arrang
+            string message = "I am in Any Mood";
+            //Act
+            MoodAnalyser analyzer = new MoodAnalyser();
+            string actual = analyzer.AnalyzerMood(message);
+            //Assert
+            Assert.AreEqual(actual, "Happy");
+        }
+    }
+}
